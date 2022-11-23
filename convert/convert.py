19312e74edc56dbf148669e766cc9b956dacdd70
minsuJo <82068871+Jominsu99@users.noreply.github.com>
2022-11-23 21:53:34 +0900

    Add files via upload

---
+++ convert/convert.py
@@ -0,0 +1,34 @@
+text_file_path = './Dense.nvm'
+
+
+with open(text_file_path,'r') as f:
+    
+    lines = f.readlines()
+    num = int(lines[2])         #3번째 줄을 읽어드린다.
+    
+with open("text.txt", 'w') as f:    
+    i = 0
+    for line in lines:
+        i = i + 1
+        if i > 3 and i < num + 4:
+            result = ""
+            c = line.split()
+    
+            t = c[0].rfind('\\')
+            x = [c[0][t:], c[6], c[7], c[8], c[2], c[3], c[4], c[5]]
+            
+            for j in x:
+                result += j + " "
+            result += "\n"
+            result1 = result.replace('\\','/')
+            f.write(result1) 
+       
+            
+        
+    
+    
+    
+    
+    
+
+        
