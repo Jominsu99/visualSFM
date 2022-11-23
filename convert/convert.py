f4499d60092b09adcb6a503c0aa1cce3e83da7c2
minsuJo <82068871+Jominsu99@users.noreply.github.com>
2022-11-23 21:58:35 +0900

    Update convert.py

--- convert/convert.py
+++ convert/convert.py
@@ -4,7 +4,7 @@ text_file_path = './Data_path.nvm'
 with open(text_file_path,'r') as f:
     
     lines = f.readlines()
-    num = int(lines[2])         #3번째 줄을 읽어드린다.
+    num = int(lines[2])         # 3번째 줄을 읽어드린다.
     
 with open("text.txt", 'w') as f:    
     i = 0
@@ -14,13 +14,13 @@ with open("text.txt", 'w') as f:
             result = ""
             c = line.split()
     
-            t = c[0].rfind('\\')
+            t = c[0].rfind('\\')    # 몇번째 이미지인지까지만 가져온다.
             x = [c[0][t:], c[6], c[7], c[8], c[2], c[3], c[4], c[5]]
             
             for j in x:
                 result += j + " "
             result += "\n"
-            result1 = result.replace('\\','/')
+            result1 = result.replace('\\','/') # 모든 \문자를 /로 바꿔준다 (리눅스에서 쓰기 위함)
             f.write(result1) 
        
             
